577bcb7252e8799f2ec102fab7826bab4d7dd8ad
Jeremy Ellis <keyfreemusic@gmail.com>
2019-06-06 22:45:26 -0700

    Update a02-re-build.sh

--- a02-re-build.sh
+++ a02-re-build.sh
@@ -29,16 +29,16 @@ cordova build android --no-telemetry
 
 
 # backup old .apk
-cp /workspace/my-gitpod-cordova/public/$wow4-app-debug.apk /workspace/my-gitpod-cordova/public/$wow4-backup-app-debug.apk
+cp /workspace/my-gitpod-dockerfile-cordova/public/$wow4-app-debug.apk /workspace/my-gitpod-dockerfile-cordova/public/$wow4-backup-app-debug.apk
 
 #copy new .apk to the public folder
-cp /workspace/my-gitpod-cordova/$wow4/platforms/android/app/build/outputs/apk/debug/app-debug.apk /workspace/my-gitpod-cordova/public/$wow4-app-debug.apk
+cp /workspace/my-gitpod-dockerfile-cordova/$wow4/platforms/android/app/build/outputs/apk/debug/app-debug.apk /workspace/my-gitpod-dockerfile-cordova/public/$wow4-app-debug.apk
 
 
 
 #printf "\n\n<a href='$wow4/platforms/android/app/build/outputs/apk/$wow4-debug.apk'>$wow4/platforms/android/app/build/outputs/apk/$wow4-debug.apk
 
-echo "<li> Next rebuild <a href="$wow4-app-debug.apk">$wow4-app-debug.apk</a><br>" >> /workspace/my-gitpod-cordova/public/index.html
+echo "<li> Next rebuild <a href="$wow4-app-debug.apk">$wow4-app-debug.apk</a><br>" >> /workspace/my-gitpod-dockerfile-cordova/public/index.html
 
 apachectl start
 
